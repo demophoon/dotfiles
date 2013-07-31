5a950ad5181857ba2cfdb97250254b81351bb797
Britt Gresham <brittcgresham@gmail.com>
2013-07-31 14:36:04 -0500

    Added other dotfiles and an install script

---
+++ bash_profile
@@ -0,0 +1,2 @@
+# Source Bashrc if file exists
+if [ -f ~/.bashrc ]; then . ~/.bashrc; fi
---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Delete Existing Dotfiles
+
+rm -rf ~/.vim
+rm -f ~/.vimrc
+rm -f ~/.bashrc
+rm -f ~/.bash_profile
+rm -f ~/.hgignore
+rm -f ~/.gitignore
+
+# Create Symlinks
+
+ln -s $DIR/vimrc ~/.vimrc
+ln -s $DIR/vim ~/.vim
+ln -s $DIR/bashrc ~/.bashrc
+ln -s $DIR/bash_profile ~/.bash_profile
+ln -s $DIR/hgignore ~/.hgignore
+ln -s $DIR/gitignore ~/.gitignore
+
+# Install Bundles using Vundle
+
+vim +BundleInstall +qall

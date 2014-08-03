af2ad2c98772bf0ea8f4ee0bf48215e8b67e0337
Britt Gresham <brittcgresham@gmail.com>
2014-08-03 21:54:41 +0000

    Removed Vim install

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 HOMEDIR=${HOME}
-ignoredfiles=(.git LICENSE.md README.md push.sh setup.sh update.sh install.sh utils .DS_Store Brewfile assets)
+ignoredfiles=(.git LICENSE.md README.md push.sh setup.sh update.sh install.sh install.py utils .DS_Store Brewfile assets)
 
 function createLinks() {
     for f in `ls -A $DIR`; do
@@ -14,7 +14,6 @@ function createLinks() {
         ln -s $DIR/$filename $HOMEDIR/$filename
     done
     source $HOMEDIR/.bashrc
-    vim +BundleClean! +BundleInstall! +qall!
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then

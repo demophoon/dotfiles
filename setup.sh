f5f0ce644c6c1c7929541611494c01809e199a8c
Britt Gresham <britt@brittg.com>
2018-09-14 17:26:27 -0700

    :sparkles: Deep merge .config directory!
    I added this feature because enough files live in .config that I need to
    start tracking them. However I don't need to sync everything in .config
    to all my machines.

--- setup.sh
+++ setup.sh
@@ -5,7 +5,11 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 HOMEDIR=${HOME:?}
+merge_directories=(.config)
 ignoredfiles=(.git .gitmodules LICENSE.md README.md push.sh setup.sh update.sh install.sh install.py utils .DS_Store Brewfile assets files Filefile)
+
+# If we force links to directories which we are deep merging, we gonna have a bad time...
+ignoredfiles+=(${merge_directories[@]})
 
 function createLinks() {
     for f in ${DIR:?}/{*,.*}; do
@@ -23,8 +27,39 @@ function updateVimPlugins() {
     vim -i NONE -c BundleInstall -c BundleClean -c qall!
 }
 
+realizeDirectories() {
+    filepath=${1:?}
+    realize_path="${HOMEDIR:?}/${filepath%/*}"
+    if [ ! -d "${realize_path:?}" ]; then
+        mkdir -p "${realize_path:?}"
+    fi
+}
+
+deepMergeDir() {
+    mergePath=${1:?}
+    if [ ! -d ${mergePath:?} ]; then
+        if [ -e ${mergePath:?} ]; then
+            dotfiles_file=${mergePath#$DIR/}
+            realizeDirectories ${dotfiles_file}
+            rm -f "${HOMEDIR:?}/${dotfiles_file:?}"
+            ln -s "${DIR:?}/${dotfiles_file:?}" "${HOMEDIR:?}/${dotfiles_file:?}"
+        fi
+    else
+        for dir in $(find ${mergePath:?}/* -maxdepth 0); do
+            deepMergeDir "${dir:?}"
+        done
+    fi
+}
+
+mergeDirs() {
+    for dir in ${merge_directories:?}; do
+        deepMergeDir "${DIR:?}/${dir:?}"
+    done
+}
+
 if [ "$1" = '-f' -o "$1" = '--force' ]; then
     createLinks
+    mergeDirs
 else
     read -n 1 -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " reply
     echo
@@ -33,6 +68,7 @@ else
 
     if [ "${reply}" = 'y' ]; then
         createLinks
+        mergeDirs
     fi
 fi
 unset createLinks

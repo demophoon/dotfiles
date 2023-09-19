abede10274182ba12795f0b351f62a8ad9fe8b6f
Britt Gresham <britt@brittg.com>
2023-09-19 12:42:52 -0700

    Update dotfile utils

--- .config/home-manager/dotfiles.nix
+++ .config/home-manager/dotfiles.nix
@@ -1,5 +1,5 @@
 builtins.fetchGit {
   url = "https://github.com/demophoon/dotfiles.git";
   ref = "main";
-  rev = "d3f83655aeeecee3e5e6639199622f617475440b";
+  rev = "cf0c5658b0b45e18fa531ade5c82b67bcd227666";
 }

29c91159e86eaeb5770b1448b2803cf54d88a6a7
Britt Gresham <britt@brittg.com>
2023-10-23 15:19:10 -0700

    Update dotfiles for zsh theme

--- .config/home-manager/dotfiles.nix
+++ .config/home-manager/dotfiles.nix
@@ -1,5 +1,5 @@
 builtins.fetchGit {
   url = "https://github.com/demophoon/dotfiles.git";
   ref = "main";
-  rev = "fd60d04ac1784aff8f44b7f8afd444eb54f7b70b";
+  rev = "6cd7a1526b7cd014b316d7244482347d8958d215";
 }

7a3c6f9acd266e22abdf48dfa9b81108d793a0ae
Britt Gresham <britt@brittg.com>
2023-11-08 09:03:30 -0800

    Update aliases

--- .config/home-manager/dotfiles.nix
+++ .config/home-manager/dotfiles.nix
@@ -1,5 +1,5 @@
 builtins.fetchGit {
   url = "https://github.com/demophoon/dotfiles.git";
   ref = "main";
-  rev = "6cd7a1526b7cd014b316d7244482347d8958d215";
+  rev = "b0195911aeb1117d0ee76f2a1d42333bea78b944";
 }

ef2a246a65eb9ad8a7b7dd84ca493b7bff7e8cdd
Britt Gresham <britt@brittg.com>
2024-02-07 10:50:45 -0800

    Add root user overrides
    Coder environments run as the root user in docker so we need to make a
    few changes to ensure that nix can be installed easily within coder.

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -1,10 +1,12 @@
 { config, pkgs, lib, builtins, ... }:
 
 {
-  home.username = "britt";
-  home.homeDirectory = "/home/britt";
+  home.username = "root";
+  home.homeDirectory = "/root";
 
   imports = [
     ./nodes/default
   ];
+
+  home.file.".localrc".source = pkgs.writeText ".localrc" "if [ -z $__first_login ]; then; export __first_login=1; cd ~ubuntu; fi; source ~ubuntu/.localrc;";
 }

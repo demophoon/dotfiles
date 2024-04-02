0e74e4405a026dcfa2df6b36e5fc14dcb1cd695f
Britt Gresham <britt@brittg.com>
2024-04-02 10:11:20 -0700

    Update dotfiles ref

--- .config/home-manager/nodes/default/dotfiles.nix
+++ .config/home-manager/nodes/default/dotfiles.nix
@@ -1,5 +1,5 @@
 builtins.fetchGit {
   url = "https://github.com/demophoon/dotfiles.git";
   ref = "main";
-  rev = "72db15623659c1032e04df838686343eba8bcbae";
+  rev = "1ccbf0f1629e3f03a8eefaa55868632232e48eff";
 }

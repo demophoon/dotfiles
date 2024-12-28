5487b59e5bd86e94be5d3b5d8503024630b5b6bb
Britt Gresham <britt@brittg.com>
2024-12-28 11:35:58 -0800

    Remove unused stratum configs

--- .config/home-manager/profiles/stratum/home.nix
+++ .config/home-manager/profiles/stratum/home.nix
@@ -1,18 +1,10 @@
 { config, pkgs, ... }:
 
-let
-  cfg = config.dotfiles.install;
-in {
+{
   home.username = "britt";
   home.homeDirectory = "/home/britt";
 
   imports = [
     ../default
-    ../../roles/sway
   ];
-
-  home.packages = with pkgs; [
-    qmk_hid
-  ];
-
 }

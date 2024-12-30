a474b5672e568a5db5d96f2dc0888ba09b1cea72
Britt Gresham <britt@brittg.com>
2024-12-30 08:39:58 -0800

    Add home-manager state to osx

--- .config/home-manager/profiles/work/home.nix
+++ .config/home-manager/profiles/work/home.nix
@@ -4,6 +4,10 @@
   home.username = "brittgresham";
   home.homeDirectory = "/Users/brittgresham";
 
+  home.stateVersion = "24.11";
+
+  programs.home-manager.enable = true;
+
   imports = [
     ../default
   ];

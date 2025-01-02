c28af6faf56b00e82a3705f5d29038d7d966631d
Britt Gresham <britt@brittg.com>
2025-01-02 09:44:55 -0800

    Remove unused configs

--- .config/regolith/i3xrocks/commands/_helpers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-roll_text() {
-    python ~/.config/regolith/i3xrocks/commands/_helpers.py roll_text "$*"
-}
-
-cycle() {
-    python ~/.config/regolith/i3xrocks/commands/_helpers.py cycle "$@"
-}

6fc2e683addbc15900e0276e250f091225770c30
Britt Gresham <britt@brittg.com>
2025-05-01 11:54:54 -0700

    feat: Add terraform_env and update set_infra
    This commit allows set_infra to be overridden with either the first
    argument as a tailscale IP. Passing an IP into this function will
    utilize consul running on that node to get the rest of the
    infrastructure IP addresses for the environment.
    
    This commit also adds in my `terraform_env` helper which loads
    environment variables from the infrastructure kv store within Vault in
    order for terraform to have all of the environment variables needed to
    run properly against my environment.

--- .config/home-manager/roles/zsh/scripts/funcs.sh
+++ .config/home-manager/roles/zsh/scripts/funcs.sh
@@ -1,7 +1,31 @@
 set_infra() {
-  export NOMAD_ADDR="https://nomad-ui.internal.demophoon.com"
-  export VAULT_ADDR="https://vault-ui.internal.demophoon.com"
-  export CONSUL_HTTP_ADDR="https://consul-ui.internal.demophoon.com"
+  export INFRA_NODE
+  INFRA_NODE=${1}
+
+  if [ -z "${1}" ]; then
+    export NOMAD_ADDR="https://nomad-ui.internal.demophoon.com"
+    export VAULT_ADDR="https://vault-ui.internal.demophoon.com"
+    export CONSUL_HTTP_ADDR="https://consul-ui.internal.demophoon.com"
+  else
+    export VAULT_SKIP_VERIFY="true"
+    export NOMAD_SKIP_VERIFY="true"
+    export CONSUL_HTTP_SSL_VERIFY=false
+
+    if type -p tailscale > /dev/null && ; then
+      if [ -z "${INFRA_NODE}" ]; then
+        INFRA_NODE=$(tailscale status --self=false --json | jq -r '.Peer | to_entries[].value | select(.Online) | .HostName' | grep -E "nuc-|proxmox-" | shuf | head -n1)
+      else
+        INFRA_NODE=$(tailscale ip ${INFRA_NODE} | head -n1)
+      fi
+    else
+      INFRA_NODE=$(dig +short ${INFRA_NODE} | grep -Po "((\d{1,3}\.){3}\d{1,3})")
+    fi
+    export VAULT_ACTIVE=$(dig @${INFRA_NODE} -p8600 +short A active.vault.service.consul.demophoon.com)
+
+    export NOMAD_ADDR="https://${INFRA_NODE}:4646"
+    export VAULT_ADDR="https://${VAULT_ACTIVE}:8200"
+    export CONSUL_HTTP_ADDR="https://${INFRA_NODE}:8501"
+  fi
 }
 
 vault_login() {
@@ -25,4 +49,12 @@ sign_public_ssh_key() {
 
 vault_ssh() {
   sign_public_ssh_key
+}
+
+terraform_env() {
+  vault_login
+  cat <({
+    source <(vault kv get -format=json -mount=kv env/infra/terraform | jq -r '.data.data | keys[] as $k | "" + $k + "=" + (.[$k]) + ";"');
+    terraform $@
+  })
 }
